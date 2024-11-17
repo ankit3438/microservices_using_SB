74cebde48efb99ca661b1784e4dd78bfdf3c943a
ankit3438 <68580225+ankit3438@users.noreply.github.com>
2024-11-17 20:10:07 +0530

    implemented api gate way and tested also running succcesfully

--- quizservice/src/main/java/com/quiz/services/QuestionClient.java
+++ quizservice/src/main/java/com/quiz/services/QuestionClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.quiz.entities.Questions;
 
 //@FeignClient(url ="http://localhost:8082",value="questionservice")
-@FeignClient(name="QUESTIONSERVICE")
+@FeignClient(name="QUESTION-SERVICE")
 public interface QuestionClient {
 
     @GetMapping("/question/quiz/{quizId}")
